5046648b309177c87d51e57a77d93f61427d2b97
test <test@fuckshadows.com>
2017-11-06 14:28:26 +0800

    try to install .NET 4.7.1 devpack

---
+++ install-devpack.cmd
@@ -0,0 +1,5 @@
+"C:\Program Files\Git\mingw64\bin\curl" -O https://download.microsoft.com/download/9/0/1/901B684B-659E-4CBD-BEC8-B3F06967C2E7/NDP471-DevPack-ENU.exe
+
+NDP471-DevPack-ENU.exe /q /norestart
+
+exit /B 0
